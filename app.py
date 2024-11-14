2ca928cfab338bdac9f4d8730413f069274ac9d1
Miranda <robertocorreiafilho0100@outlook.com>
2024-11-14 14:59:03 -0300

    feat: atualizacoes

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'username': 'admin',
+    'password': 'admin'
+}
+
 api = Api(app)
+db = MongoEngine(app)
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
     def get(self):
-        return {'message': 'user 1'}
+        return UserModel.objects()
+        # return {'message': 'user 1'}
 
 
 class User(Resource):
@@ -22,4 +42,4 @@ api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=5001)
